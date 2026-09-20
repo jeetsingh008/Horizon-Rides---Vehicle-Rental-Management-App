9608e9bdc40797ca25ea3cfd8959a37d95eeb738
jeetsingh008 <158179537+jeetsingh008@users.noreply.github.com>
2026-09-20 18:29:48 +0530

    first commit

---
+++ src/app/pages/customer-listing/customer-listing.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-listing',
+  imports: [],
+  templateUrl: './customer-listing.html',
+  styleUrl: './customer-listing.css',
+})
+export class CustomerListing {}
---
+++ src/app/pages/vehicle-master/vehicle-master.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-master',
+  imports: [],
+  templateUrl: './vehicle-master.html',
+  styleUrl: './vehicle-master.css',
+})
+export class VehicleMaster {}
